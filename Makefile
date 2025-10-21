969fbaf414cf33f886739abe3d4657d3d65f56c0
zhenkai.sun <zhenkai.sun@qq.com>
2025-10-21 14:33:29 +0800

    add compile_commands generator

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-.PHONY: build
+.PHONY: build refresh
 
 build:
 	@bazel build //cmd:hello
+
+refresh:
+	@bazel run @hedron_compile_commands//:refresh_all
--- lib/htime.cc
+++ lib/htime.cc
@@ -1,9 +1,12 @@
 #include <ctime>
 #include <iostream>
+// 3rd
+#include "fmt/format.h"
 // self
 #include "lib/htime.h"
 
 void print_localtime() {
   std::time_t result = std::time(nullptr);
   std::cout << std::asctime(std::localtime(&result));
+  std::cout << fmt::format("{}", std::asctime(std::localtime(&result)));
 }
